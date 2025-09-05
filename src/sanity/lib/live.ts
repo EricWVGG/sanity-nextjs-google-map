4150bfebaef30477f436a1535f9e13b34d7e1ad3
Eric Jacobsen <eric@whiskyvangoghgo.com>
2025-09-04 23:56:20 -0400

    cleanup

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,17 +1,9 @@
-// note: this only works in components that generate user-facing data (pages), due to use of preview/drafts
-// for generateStaticParams, use ../client.ts
-
-import { defineLive } from 'next-sanity'
-import { client } from './client'
-import { token } from '../env'
+import { defineLive } from "next-sanity"
+import { client } from "./client"
+import { apiVersion } from "../env"
 
 export const { sanityFetch, SanityLive } = defineLive({
-  // @ts-ignore
   client: client.withConfig({
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX',
+    apiVersion,
   }),
-  serverToken: token,
-  browserToken: token,
 })
